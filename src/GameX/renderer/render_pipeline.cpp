1076ff4dfc45bed1608f0096cd246b91bcb0f997
LazyJazz <zijian.lv@hotmail.com>
2023-12-24 10:37:51 +0800

    Add lighting subpass

--- src/GameX/renderer/render_pipeline.cpp
+++ src/GameX/renderer/render_pipeline.cpp
@@ -9,86 +9,10 @@
 namespace GameX {
 RenderPipeline::RenderPipeline(struct Renderer *renderer, int max_film)
     : renderer_(renderer) {
-  std::vector<VkAttachmentDescription> attachment_descriptions;
-  attachment_descriptions.push_back(VkAttachmentDescription{
-      0, VK_FORMAT_R32G32B32A32_SFLOAT, VK_SAMPLE_COUNT_1_BIT,
-      VK_ATTACHMENT_LOAD_OP_CLEAR, VK_ATTACHMENT_STORE_OP_STORE,
-      VK_ATTACHMENT_LOAD_OP_DONT_CARE, VK_ATTACHMENT_STORE_OP_DONT_CARE,
-      VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL});
-  attachment_descriptions.push_back(VkAttachmentDescription{
-      0, VK_FORMAT_R32G32B32A32_SFLOAT, VK_SAMPLE_COUNT_1_BIT,
-      VK_ATTACHMENT_LOAD_OP_CLEAR, VK_ATTACHMENT_STORE_OP_STORE,
-      VK_ATTACHMENT_LOAD_OP_DONT_CARE, VK_ATTACHMENT_STORE_OP_DONT_CARE,
-      VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL});
-  attachment_descriptions.push_back(VkAttachmentDescription{
-      0, VK_FORMAT_R32G32B32A32_SFLOAT, VK_SAMPLE_COUNT_1_BIT,
-      VK_ATTACHMENT_LOAD_OP_CLEAR, VK_ATTACHMENT_STORE_OP_STORE,
-      VK_ATTACHMENT_LOAD_OP_DONT_CARE, VK_ATTACHMENT_STORE_OP_DONT_CARE,
-      VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL});
-  attachment_descriptions.push_back(VkAttachmentDescription{
-      0, VK_FORMAT_D32_SFLOAT, VK_SAMPLE_COUNT_1_BIT,
-      VK_ATTACHMENT_LOAD_OP_CLEAR, VK_ATTACHMENT_STORE_OP_STORE,
-      VK_ATTACHMENT_LOAD_OP_DONT_CARE, VK_ATTACHMENT_STORE_OP_DONT_CARE,
-      VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL});
-
-  std::vector<VkAttachmentReference> color_attachment_references;
-  color_attachment_references.push_back(
-      VkAttachmentReference{0, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL});
-  color_attachment_references.push_back(
-      VkAttachmentReference{1, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL});
-  color_attachment_references.push_back(
-      VkAttachmentReference{2, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL});
-
-  std::optional<VkAttachmentReference> depth_attachment_reference =
-      VkAttachmentReference{3,
-                            VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL};
-
-  geometry_pass_render_pass_ = std::make_unique<grassland::vulkan::RenderPass>(
-      renderer->App()->Core(), attachment_descriptions,
-      color_attachment_references, depth_attachment_reference);
-
-  geometry_pass_vertex_shader_ =
-      std::make_unique<grassland::vulkan::ShaderModule>(
-          renderer->App()->Core(), BuiltInShaderSpv("geometry_pass.vert"));
-  geometry_pass_fragment_shader_ =
-      std::make_unique<grassland::vulkan::ShaderModule>(
-          renderer->App()->Core(), BuiltInShaderSpv("geometry_pass.frag"));
-
-  geometry_pass_pipeline_layout_ =
-      std::make_unique<grassland::vulkan::PipelineLayout>(
-          renderer->App()->Core(),
-          std::vector<grassland::vulkan::DescriptorSetLayout *>{
-              renderer->CameraDescriptorSetLayout(),
-              renderer->EntityDescriptorSetLayout()});
-
-  grassland::vulkan::PipelineSettings geometry_pass_pipeline_settings(
-      geometry_pass_render_pass_.get(), geometry_pass_pipeline_layout_.get());
-  geometry_pass_pipeline_settings.AddShaderStage(
-      geometry_pass_vertex_shader_.get(), VK_SHADER_STAGE_VERTEX_BIT);
-  geometry_pass_pipeline_settings.AddShaderStage(
-      geometry_pass_fragment_shader_.get(), VK_SHADER_STAGE_FRAGMENT_BIT);
-  geometry_pass_pipeline_settings.AddInputBinding(0, sizeof(Vertex),
-                                                  VK_VERTEX_INPUT_RATE_VERTEX);
-  geometry_pass_pipeline_settings.AddInputAttribute(
-      0, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, position));
-  geometry_pass_pipeline_settings.AddInputAttribute(
-      0, 1, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, color));
-  geometry_pass_pipeline_settings.AddInputAttribute(
-      0, 2, VK_FORMAT_R32G32_SFLOAT, offsetof(Vertex, texCoord));
-  geometry_pass_pipeline_settings.AddInputAttribute(
-      0, 3, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, normal));
-  geometry_pass_pipeline_settings.AddInputAttribute(
-      0, 4, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, tangent));
-
-  geometry_pass_pipeline_settings.SetPrimitiveTopology(
-      VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST);
-
-  geometry_pass_pipeline_settings.SetMultiSampleState(VK_SAMPLE_COUNT_1_BIT);
-
-  geometry_pass_pipeline_settings.SetCullMode(VK_CULL_MODE_FRONT_BIT);
-
-  geometry_pass_pipeline_ = std::make_unique<grassland::vulkan::Pipeline>(
-      renderer->App()->Core(), geometry_pass_pipeline_settings);
+  CreateRenderPass();
+  CreateGeometryPass();
+  CreateLightingPassCommonAssets(max_film);
+  CreateLightAmbientPipeline();
 }
 
 std::unique_ptr<RenderPipeline::Film> RenderPipeline::CreateFilm(int width,
@@ -99,25 +23,40 @@ std::unique_ptr<RenderPipeline::Film> RenderPipeline::CreateFilm(int width,
   film->albedo_image = std::make_unique<grassland::vulkan::Image>(
       renderer_->App()->Core(), VK_FORMAT_R32G32B32A32_SFLOAT, extent,
       VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT |
-          VK_IMAGE_USAGE_TRANSFER_SRC_BIT);
+          VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
+          VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT);
   film->normal_image = std::make_unique<grassland::vulkan::Image>(
       renderer_->App()->Core(), VK_FORMAT_R32G32B32A32_SFLOAT, extent,
       VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT |
-          VK_IMAGE_USAGE_TRANSFER_SRC_BIT);
+          VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
+          VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT);
   film->position_image = std::make_unique<grassland::vulkan::Image>(
       renderer_->App()->Core(), VK_FORMAT_R32G32B32A32_SFLOAT, extent,
       VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT |
-          VK_IMAGE_USAGE_TRANSFER_SRC_BIT);
+          VK_IMAGE_USAGE_TRANSFER_SRC_BIT |
+          VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT);
   film->depth_image = std::make_unique<grassland::vulkan::Image>(
       renderer_->App()->Core(), VK_FORMAT_D32_SFLOAT, extent,
       VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT |
-          VK_IMAGE_USAGE_TRANSFER_SRC_BIT,
+          VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT,
       VK_IMAGE_ASPECT_DEPTH_BIT);
+
+  film->output_image = std::make_unique<grassland::vulkan::Image>(
+      renderer_->App()->Core(), VK_FORMAT_R32G32B32A32_SFLOAT, extent,
+      VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_SAMPLED_BIT |
+          VK_IMAGE_USAGE_TRANSFER_SRC_BIT);
+
   film->framebuffer = std::make_unique<grassland::vulkan::Framebuffer>(
-      renderer_->App()->Core(), extent, geometry_pass_render_pass_->Handle(),
+      renderer_->App()->Core(), extent, render_pass_->Handle(),
       std::vector<VkImageView>{
           film->albedo_image->ImageView(), film->normal_image->ImageView(),
           film->position_image->ImageView(), film->depth_image->ImageView()});
+
+  film->input_attachment_set =
+      std::make_unique<grassland::vulkan::DescriptorSet>(
+          renderer_->App()->Core(), lighting_pass_descriptor_pool_.get(),
+          lighting_pass_descriptor_set_layout_.get());
+
   return film;
 }
 
@@ -127,7 +66,7 @@ void RenderPipeline::Render(VkCommandBuffer cmd_buffer,
                             const RenderPipeline::Film &film) {
   VkRenderPassBeginInfo render_pass_begin_info{};
   render_pass_begin_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-  render_pass_begin_info.renderPass = geometry_pass_render_pass_->Handle();
+  render_pass_begin_info.renderPass = render_pass_->Handle();
   render_pass_begin_info.framebuffer = film.framebuffer->Handle();
   render_pass_begin_info.renderArea.offset = {0, 0};
   render_pass_begin_info.renderArea.extent = film.Extent();
@@ -188,6 +127,153 @@ void RenderPipeline::Render(VkCommandBuffer cmd_buffer,
     vkCmdDrawIndexed(cmd_buffer, entity->Model()->IndexCount(), 1, 0, 0, 0);
   }
 
+  vkCmdNextSubpass(cmd_buffer, VK_SUBPASS_CONTENTS_INLINE);
+
   vkCmdEndRenderPass(cmd_buffer);
+}
+
+void RenderPipeline::CreateRenderPass() {
+  std::vector<VkAttachmentDescription> attachment_descriptions;
+  attachment_descriptions.push_back(VkAttachmentDescription{
+      0, VK_FORMAT_R32G32B32A32_SFLOAT, VK_SAMPLE_COUNT_1_BIT,
+      VK_ATTACHMENT_LOAD_OP_CLEAR, VK_ATTACHMENT_STORE_OP_STORE,
+      VK_ATTACHMENT_LOAD_OP_DONT_CARE, VK_ATTACHMENT_STORE_OP_DONT_CARE,
+      VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL});
+  attachment_descriptions.push_back(VkAttachmentDescription{
+      0, VK_FORMAT_R32G32B32A32_SFLOAT, VK_SAMPLE_COUNT_1_BIT,
+      VK_ATTACHMENT_LOAD_OP_CLEAR, VK_ATTACHMENT_STORE_OP_STORE,
+      VK_ATTACHMENT_LOAD_OP_DONT_CARE, VK_ATTACHMENT_STORE_OP_DONT_CARE,
+      VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL});
+  attachment_descriptions.push_back(VkAttachmentDescription{
+      0, VK_FORMAT_R32G32B32A32_SFLOAT, VK_SAMPLE_COUNT_1_BIT,
+      VK_ATTACHMENT_LOAD_OP_CLEAR, VK_ATTACHMENT_STORE_OP_STORE,
+      VK_ATTACHMENT_LOAD_OP_DONT_CARE, VK_ATTACHMENT_STORE_OP_DONT_CARE,
+      VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL});
+  attachment_descriptions.push_back(VkAttachmentDescription{
+      0, VK_FORMAT_D32_SFLOAT, VK_SAMPLE_COUNT_1_BIT,
+      VK_ATTACHMENT_LOAD_OP_CLEAR, VK_ATTACHMENT_STORE_OP_STORE,
+      VK_ATTACHMENT_LOAD_OP_DONT_CARE, VK_ATTACHMENT_STORE_OP_DONT_CARE,
+      VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL});
+
+  std::vector<VkAttachmentReference> color_attachment_references;
+  color_attachment_references.push_back(
+      VkAttachmentReference{0, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL});
+  color_attachment_references.push_back(
+      VkAttachmentReference{1, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL});
+  color_attachment_references.push_back(
+      VkAttachmentReference{2, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL});
+
+  std::optional<VkAttachmentReference> depth_attachment_reference =
+      VkAttachmentReference{3,
+                            VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL};
+
+  std::vector<grassland::vulkan::SubpassSettings> subpass_settings;
+  subpass_settings.emplace_back(
+      std::vector<VkAttachmentReference>{
+          {0, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL},
+          {1, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL},
+          {2, VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL},
+      },
+      std::optional<VkAttachmentReference>(
+          {3, VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL}),
+      std::vector<VkAttachmentReference>{});
+
+  // Add lighting subpass
+  subpass_settings.emplace_back(
+      std::vector<VkAttachmentReference>{
+          {0, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL},
+          {1, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL},
+          {2, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL},
+      },
+      std::vector<VkAttachmentReference>{}, std::nullopt,
+      std::vector<VkAttachmentReference>{}, std::vector<uint32_t>{3});
+
+  std::vector<VkSubpassDependency> dependencies;
+  dependencies.push_back(VkSubpassDependency{
+      VK_SUBPASS_EXTERNAL, 0, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
+      VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT, VK_ACCESS_SHADER_READ_BIT,
+      VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT, VK_DEPENDENCY_BY_REGION_BIT});
+
+  dependencies.push_back(VkSubpassDependency{
+      0, 1, VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+      VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT,
+      VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT, VK_ACCESS_SHADER_READ_BIT,
+      VK_DEPENDENCY_BY_REGION_BIT});
+
+  render_pass_ = std::make_unique<grassland::vulkan::RenderPass>(
+      renderer_->App()->Core(), attachment_descriptions, subpass_settings,
+      dependencies);
+}
+
+void RenderPipeline::CreateGeometryPass() {
+  geometry_pass_vertex_shader_ =
+      std::make_unique<grassland::vulkan::ShaderModule>(
+          renderer_->App()->Core(), BuiltInShaderSpv("geometry_pass.vert"));
+  geometry_pass_fragment_shader_ =
+      std::make_unique<grassland::vulkan::ShaderModule>(
+          renderer_->App()->Core(), BuiltInShaderSpv("geometry_pass.frag"));
+
+  geometry_pass_pipeline_layout_ =
+      std::make_unique<grassland::vulkan::PipelineLayout>(
+          renderer_->App()->Core(),
+          std::vector<grassland::vulkan::DescriptorSetLayout *>{
+              renderer_->CameraDescriptorSetLayout(),
+              renderer_->EntityDescriptorSetLayout()});
+
+  grassland::vulkan::PipelineSettings geometry_pass_pipeline_settings(
+      render_pass_.get(), geometry_pass_pipeline_layout_.get());
+  geometry_pass_pipeline_settings.AddShaderStage(
+      geometry_pass_vertex_shader_.get(), VK_SHADER_STAGE_VERTEX_BIT);
+  geometry_pass_pipeline_settings.AddShaderStage(
+      geometry_pass_fragment_shader_.get(), VK_SHADER_STAGE_FRAGMENT_BIT);
+  geometry_pass_pipeline_settings.AddInputBinding(0, sizeof(Vertex),
+                                                  VK_VERTEX_INPUT_RATE_VERTEX);
+  geometry_pass_pipeline_settings.AddInputAttribute(
+      0, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, position));
+  geometry_pass_pipeline_settings.AddInputAttribute(
+      0, 1, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, color));
+  geometry_pass_pipeline_settings.AddInputAttribute(
+      0, 2, VK_FORMAT_R32G32_SFLOAT, offsetof(Vertex, texCoord));
+  geometry_pass_pipeline_settings.AddInputAttribute(
+      0, 3, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, normal));
+  geometry_pass_pipeline_settings.AddInputAttribute(
+      0, 4, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, tangent));
+
+  geometry_pass_pipeline_settings.SetPrimitiveTopology(
+      VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST);
+
+  geometry_pass_pipeline_settings.SetMultiSampleState(VK_SAMPLE_COUNT_1_BIT);
+
+  geometry_pass_pipeline_settings.SetCullMode(VK_CULL_MODE_BACK_BIT);
+
+  geometry_pass_pipeline_ = std::make_unique<grassland::vulkan::Pipeline>(
+      renderer_->App()->Core(), geometry_pass_pipeline_settings);
+}
+
+void RenderPipeline::CreateLightingPassCommonAssets(int max_film) {
+  lighting_pass_descriptor_pool_ =
+      std::make_unique<grassland::vulkan::DescriptorPool>(
+          renderer_->App()->Core(),
+          std::vector<VkDescriptorPoolSize>{
+              {VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT,
+               static_cast<uint32_t>(max_film *
+                                     Film::kNumGBufferInputAttachments)},
+          },
+          max_film);
+
+  lighting_pass_descriptor_set_layout_ =
+      std::make_unique<grassland::vulkan::DescriptorSetLayout>(
+          renderer_->App()->Core(),
+          std::vector<VkDescriptorSetLayoutBinding>{
+              {0, VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT, 1,
+               VK_SHADER_STAGE_FRAGMENT_BIT, nullptr},
+              {1, VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT, 1,
+               VK_SHADER_STAGE_FRAGMENT_BIT, nullptr},
+              {2, VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT, 1,
+               VK_SHADER_STAGE_FRAGMENT_BIT, nullptr},
+          });
+}
+
+void RenderPipeline::CreateLightAmbientPipeline() {
 }
 }  // namespace GameX
--- src/GameX/renderer/render_pipeline.h
+++ src/GameX/renderer/render_pipeline.h
@@ -9,11 +9,16 @@ class RenderPipeline {
   RenderPipeline(class Renderer *renderer, int max_film = 1024);
 
   struct Film {
+    static constexpr int kNumGBufferInputAttachments = 3;
     std::unique_ptr<grassland::vulkan::Image> albedo_image;
     std::unique_ptr<grassland::vulkan::Image> normal_image;
     std::unique_ptr<grassland::vulkan::Image> position_image;
     std::unique_ptr<grassland::vulkan::Image> depth_image;
+
+    std::unique_ptr<grassland::vulkan::Image> output_image;
+
     std::unique_ptr<grassland::vulkan::Framebuffer> framebuffer;
+    std::unique_ptr<grassland::vulkan::DescriptorSet> input_attachment_set;
 
     VkExtent2D Extent() const {
       return albedo_image->Extent();
@@ -28,21 +33,26 @@ class RenderPipeline {
               const Film &film);
 
  private:
+  void CreateRenderPass();
+  void CreateGeometryPass();
+  void CreateLightingPassCommonAssets(int max_film);
+  void CreateLightAmbientPipeline();
+
   class Renderer *renderer_;
+  std::unique_ptr<grassland::vulkan::RenderPass> render_pass_;
   std::unique_ptr<grassland::vulkan::ShaderModule> geometry_pass_vertex_shader_;
   std::unique_ptr<grassland::vulkan::ShaderModule>
       geometry_pass_fragment_shader_;
-  std::unique_ptr<grassland::vulkan::ShaderModule> lighting_pass_vertex_shader_;
-  std::unique_ptr<grassland::vulkan::ShaderModule>
-      lighting_pass_fragment_shader_;
-  std::unique_ptr<grassland::vulkan::RenderPass> geometry_pass_render_pass_;
-  std::unique_ptr<grassland::vulkan::RenderPass> lighting_pass_render_pass_;
   std::unique_ptr<grassland::vulkan::PipelineLayout>
       geometry_pass_pipeline_layout_;
-  std::unique_ptr<grassland::vulkan::PipelineLayout>
-      lighting_pass_pipeline_layout_;
   std::unique_ptr<grassland::vulkan::Pipeline> geometry_pass_pipeline_;
-  std::unique_ptr<grassland::vulkan::Pipeline> lighting_pass_pipeline_;
+
+  std::unique_ptr<grassland::vulkan::ShaderModule> light_ambient_vertex_shader_;
+  std::unique_ptr<grassland::vulkan::ShaderModule>
+      light_ambient_fragment_shader_;
+  std::unique_ptr<grassland::vulkan::PipelineLayout>
+      light_ambient_pipeline_layout_;
+  std::unique_ptr<grassland::vulkan::Pipeline> light_ambient_pipeline_;
 
   std::unique_ptr<grassland::vulkan::DescriptorSetLayout>
       lighting_pass_descriptor_set_layout_;
